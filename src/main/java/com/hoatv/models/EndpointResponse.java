171be4e344422a4085d62bcf367b79ba49541412
hoatranv <tranvanhoactk34@gmail.com>
2021-11-20 15:16:50 +0700

    Change generated value strategy to identity

--- src/main/java/com/hoatv/models/EndpointResponse.java
+++ src/main/java/com/hoatv/models/EndpointResponse.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 public class EndpointResponse {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
