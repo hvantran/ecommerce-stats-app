f95d057652ba19a8c980efea4766bdd7f9631c9a
hoatranv <tranvanhoactk34@gmail.com>
2021-12-23 22:28:05 +0700

    Adding Swagger UI to ecommerce project

--- src/main/java/com/hoatv/Application.java
+++ src/main/java/com/hoatv/Application.java
@@ -16,6 +16,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,6 +30,15 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
     @Bean(destroyMethod = "destroy")
