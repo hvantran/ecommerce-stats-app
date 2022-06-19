325661e26c2b98dba16caba32d45e8c7942eab18
hoatranv <tranvanhoactk34@gmail.com>
2022-06-20 01:07:13 +0700

    Adding springboot-starter-test

---
+++ src/test/java/com/hoatv/ApplicationTest.java
@@ -0,0 +1,16 @@
+package com.hoatv;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void testRunAsExpected() {
+        assertThat(1, Matchers.equalTo(1));
+    }
+}
