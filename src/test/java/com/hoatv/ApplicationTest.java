f2effdc7c05087295b0dc35b059bfd217cf645e2
hoatranv <tranvanhoactk34@gmail.com>
2022-07-05 10:42:51 +0700

    Remove sample test

--- src/test/java/com/hoatv/ApplicationTest.java
+++ src/test/java/com/hoatv/ApplicationTest.java
@@ -1,21 +1,8 @@
 package com.hoatv;
 
-import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.hamcrest.MatcherAssert.assertThat;
 
 @SpringBootTest
 class ApplicationTest {
 
-    @Test
-    void testRunAsExpected() {
-        assertThat(1, Matchers.equalTo(1));
-    }
-
-    @Test
-    void testRunAsFailed() {
-        assertThat(1, Matchers.equalTo(2));
-    }
 }
