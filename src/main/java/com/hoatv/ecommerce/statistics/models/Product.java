df7edacf0df7f137c08873d4032fba4a7c307393
hoatranv <hoa.tranvan@unified.com>
2023-04-05 12:24:13 +0700

    Upgrade spring boot version to 3.0.5

--- src/main/java/com/hoatv/ecommerce/statistics/models/Product.java
+++ src/main/java/com/hoatv/ecommerce/statistics/models/Product.java
@@ -1,10 +1,10 @@
 package com.hoatv.ecommerce.statistics.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
 
 @Entity
 @Getter
