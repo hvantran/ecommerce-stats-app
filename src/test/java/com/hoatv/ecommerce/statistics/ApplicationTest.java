a957dfa2d0919c26ffccbf7265f1378e0c594fd2
hvantran <tranvanhoactk34@gmail.com>
2025-08-26 15:50:32 +0700

    Fix build failure

--- src/test/java/com/hoatv/ecommerce/statistics/ApplicationTest.java
+++ src/test/java/com/hoatv/ecommerce/statistics/ApplicationTest.java
@@ -3,10 +3,12 @@ package com.hoatv.ecommerce.statistics;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @SpringBootTest(classes=EcommerceStatisticsApplication.class)
+@ActiveProfiles("test")
 class ApplicationTest {
 
     @Test
